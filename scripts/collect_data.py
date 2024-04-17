7d3b88fd1e19a99f83b26c6a336450d616f2a9d1
fernandopalafox <fernandopalafox@utexas.edu>
2024-04-17 14:06:18 -0500

    overhauled collect data with help from jake

--- scripts/collect_data.py
+++ scripts/collect_data.py
@@ -3,16 +3,20 @@ from jax import numpy as jp
 import robosuite as suite
 from robosuite.controllers import load_controller_config
 from brax.envs.panda import Panda
+import flax
+from brax.envs import State
 import numpy as np
 import time
+import pickle
 
 # -----------------------
 # --- Sim Parameters ----
 # -----------------------
-num_steps = 1024
+num_steps = 256
 friction_torque_coeff = 0.1
 friction_static = 0.5
 num_joints = 7
+key = jax.random.key(0)
 
 # -----------------------
 # --- Initial states ----
@@ -76,17 +80,16 @@ env_reset_jitted = jax.jit(env_brax.reset)
 env_step_jitted = jax.jit(env_brax.step)
 brax_init_state = env_reset_jitted(jax.random.PRNGKey(seed))
 env_set_state_jitted = jax.jit(env_brax.set_state)
+low, high = env_suite.action_spec
 
 print("Brax environment loaded.")
 print(f"Time taken: {time.time() - start_time}")
 
-# Save initial state, torque_osc, torque_friction, and new state for each step
-data_states_init = []
-data_torques = []
-data_friction = []
-data_states_new = []
-
 # Set initial state in both environments
+
+
+print("Setting initial state in both environments...")
+start_time = time.time()
 
 # Robosuite
 env_suite.sim.data.qpos[env_suite.robots[0].joint_indexes] = q_initial
@@ -95,48 +98,41 @@ env_suite.sim.data.qvel[env_suite.robots[0].joint_indexes] = qd_initial
 # Brax
 brax_init_state = env_set_state_jitted(q_initial, qd_initial)
 
-# Data collection loop
-start_time = time.time()
-print("Starting data collection loop...")
-for step in range(num_steps):
+print("Initial state set.")
+print(f"Time taken: {time.time() - start_time}")
 
-    # --- Act in both environmenst ---
+# Data class
+@flax.struct.dataclass
+class MyData:
+    init_state: State
+    torque: jp.ndarray
+    friction: jp.ndarray
+    next_state: State
 
-    # Sample random perturbation (from current state in operational space)
-    low, high = env_suite.action_spec
+
+# Data collection function
+def make_data(key):
+    init_state = env_reset_jitted(key)
     action = np.random.uniform(low, high)
-
-    # Step Robosuite environment
     _, _, _, _ = env_suite.step(action)
-
-    # Extract corresponding OSC controller torques
     torques_osc = env_suite.sim.data.ctrl
-
-    # Compute friction torques given the initial state
     torques_friction = compute_friction_torques(
         brax_init_state.pipeline_state.q, brax_init_state.pipeline_state.qd
     )
-
-    # Step Brax environment with osc torques + friction torques
     torques_total = (
         torques_osc[env_suite.robots[0].joint_indexes] + torques_friction
     )
-    brax_new_state = env_step_jitted(brax_init_state, torques_total)
+    next_state = env_step_jitted(brax_init_state, torques_total)
 
-    # Save data
-    data_states_init.append(brax_new_state)
-    data_torques.append(torques_osc[0:num_joints])
-    data_friction.append(torques_friction)
-    data_states_new.append(brax_new_state)
-    print(f"Step: {step}")
+    return MyData(init_state, torques_osc, torques_friction, next_state)
 
-    # Update initial state
-    brax_init_state = brax_new_state
 
+# Collect data
+print("Starting data collection loop...")
+data = jax.vmap(make_data)(jax.random.split(key, num=num_steps))
 print(f"Time taken: {time.time() - start_time}"),
 
-# Save data to file
-np.save("data/data_states_init.npy", data_states_init)
-np.save("data/data_torques.npy", data_torques)
-np.save("data/data_friction.npy", data_friction)
-np.save("data/data_states_new.npy", data_states_new)
+
+# Save data
+with open('data/data.pkl', 'wb') as f:
+    pickle.dump(data, f)
