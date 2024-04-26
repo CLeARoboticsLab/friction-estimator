351514b036acc2d1d9299098b122059edc2031a9
fernandopalafox <fernandopalafox@utexas.edu>
2024-04-26 11:16:55 -0500

    simple robosuite controller works

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -12,6 +12,8 @@ from brax.envs import State
 from jax import numpy as jp
 import numpy as np
 from flax import serialization
+import imageio
+import cv2
 
 
 # -----------------------
@@ -29,7 +31,7 @@ seed = 0
 friction_torque_coeff = 10.
 friction_static = 0.05
 
-num_steps = 2000
+num_steps = 200
 torque_logging_interval = 10
 key = jax.random.key(0)
 key_states = jax.random.split(key, num=num_steps)
@@ -49,6 +51,8 @@ timestep_length = 0.002
 
 perturbation_index = 1
 perturbation_amount = 0.1
+
+plot_lim = 0.5
 
 # -----------------------
 # --- Friction model ----
@@ -90,44 +94,33 @@ config = load_controller_config(default_controller="OSC_POSE")
 env_config = {}
 env_config["env_name"] = "Lift"
 env_config["robots"] = "Panda"
-env_config["camera_names"] = ["frontview"]
+env_config["camera_names"] = ["sideview"]
 env_config["camera_heights"] = 480
 env_config["camera_widths"] = 480
 env_config["control_freq"] = 10
-env_config["controller_configs"] = suite.load_controller_config(
-    default_controller="OSC_POSE"
-)
 env_config["has_renderer"] = False
-env_config["has_offscreen_renderer"] = False
+env_config["has_offscreen_renderer"] = True
 env_config["ignore_done"] = True
-env_config["use_camera_obs"] = False
+env_config["use_camera_obs"] = True
+
+controller_config = suite.load_controller_config(
+    default_controller="OSC_POSITION"
+)
+controller_config['control_delta'] = True
+env_config["controller_configs"] = controller_config
 
 # Make the environment
 env_suite = suite.make(**env_config)
-env_suite_corrected = suite.make(**env_config)
+env_suite.sim.model.opt.timestep = timestep_length
 
 # Reset the environment
-env_suite.reset()
-env_suite_corrected.reset()
+obs = env_suite.reset()
+
+# TEMPORARY: Set initial state
+env_suite.sim.data.qpos[env_suite.robots[0].joint_indexes] = initial_q
+env_suite.sim.data.qvel[env_suite.robots[0].joint_indexes] = initial_qd
 
 print(f"Robosuite environment loaded. Time taken: {time.time() - start_time}")
-
-# -----------------------
-# --- Brax stuff --------
-# -----------------------
-
-print("Loading Brax environment...")
-start_time = time.time()
-
-# Setup Brax environment
-seed = 0
-env_brax = Panda()
-env_reset_jitted = jax.jit(env_brax.reset)
-env_set_state_jitted = jax.jit(env_brax.set_state)
-env_step_jitted = jax.jit(env_brax.step)
-low, high = env_suite.action_spec
-
-print(f"Brax environment loaded. Time taken: {time.time() - start_time}")
 
 # ----------------------------
 # --- Run tracking -----------
@@ -136,81 +129,33 @@ print("Testing tracking...")
 start_time = time.time()
 
 # Action
-action = np.zeros(num_joints)
+action = np.zeros(3 + 1)
 cartesian_perturbation = np.zeros(3)
 cartesian_perturbation[perturbation_index] = perturbation_amount
 action[0:3] = cartesian_perturbation
 
-# Sample brax env
-init_state = env_set_state_jitted(initial_q, initial_qd)
-init_state_corrected = init_state
 
 # Run the simulation
-ee_positions_vanilla = [init_state.pipeline_state.x.pos[6][perturbation_index]]
-ee_positions_corrected = [
-    init_state.pipeline_state.x.pos[6][perturbation_index]
-]
+ee_positions_robosuite = []
+frames = []
+ee_positions_robosuite.append(obs['robot0_eef_pos'])
+frames.append(obs["sideview_image"])
 for i in range(num_steps):
 
-    # Set robosuite envs
-    env_suite.sim.data.qpos[env_suite.robots[0].joint_indexes] = (
-        init_state.pipeline_state.q
-    )
-    env_suite.sim.data.qvel[env_suite.robots[0].joint_indexes] = (
-        init_state.pipeline_state.qd
-    )
-    env_suite_corrected.sim.data.qpos[env_suite.robots[0].joint_indexes] = (
-        init_state_corrected.pipeline_state.q
-    )
-    env_suite_corrected.sim.data.qvel[env_suite.robots[0].joint_indexes] = (
-        init_state_corrected.pipeline_state.qd
-    )
-
     # Step robosuite and get control torques
-    _, _, _, _ = env_suite.step(action)
-    torques_control = env_suite.sim.data.ctrl
-    torques_control = torques_control[0:num_joints]
+    obs, _, _, _ = env_suite.step(action)
 
-    _, _, _, _ = env_suite_corrected.step(action)
-    torques_control_corrected = env_suite_corrected.sim.data.ctrl
-    torques_control_corrected = torques_control[0:num_joints]
+    # Save end effecttor position within robosuite
+    ee_positions_robosuite.append(obs['robot0_eef_pos'])
 
-    # Compute friction torques
-    torques_friction = compute_friction_torques(
-        init_state.pipeline_state.q, init_state.pipeline_state.qd
-    )
-
-    torques_friction_corrected = compute_friction_torques(
-        init_state_corrected.pipeline_state.q,
-        init_state_corrected.pipeline_state.qd,
-    )
-
-    # Evaluate the model
-    torques_compensation = network.apply(
-        loaded_params, init_state_corrected.obs
-    )
-
-    # Step in both brax envs
-    init_state = env_step_jitted(
-        init_state, torques_control
-    )
-    init_state_corrected = env_step_jitted(
-        init_state_corrected,
-        torques_control_corrected
-    )
-
-    # Save end effector position
-    ee_positions_vanilla.append(
-        init_state.pipeline_state.x.pos[6][perturbation_index]
-    )
-    ee_positions_corrected.append(
-        init_state_corrected.pipeline_state.x.pos[6][perturbation_index]
-    )
+    # Record frame 
+    frames.append(obs["sideview_image"])
 
     # Log
     if i % torque_logging_interval == 0:
         print(f"Step {i} of {num_steps}")
 
+ee_positions_robosuite = np.array(ee_positions_robosuite)
 
 print(f"Tracking done. Time taken: {time.time() - start_time}")
 
@@ -221,12 +166,56 @@ print(f"Tracking done. Time taken: {time.time() - start_time}")
 total_perturbation = perturbation_amount * num_steps * timestep_length
 time_vec = np.arange(num_steps + 1) * timestep_length
 
-plt.figure()
-plt.plot(time_vec, ee_positions_vanilla, label="Vanilla")
-plt.plot(time_vec, ee_positions_corrected, label="Corrected")
-# plt.axhline(y=total_perturbation, color='r', linestyle='--', label='Reference')
-plt.legend()
-plt.xlabel("Time")
-plt.ylabel(f"End effector {['x','y','z'][perturbation_index]}-position [m]")
-plt.title("Tracking performance")
+# Create subplots
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+axs[0].plot(ee_positions_robosuite[:, 0], ee_positions_robosuite[:, 1])
+axs[0].set_xlabel("End effector x-position [m]")
+axs[0].set_ylabel("End effector y-position [m]")
+axs[0].set_xlim(-plot_lim, plot_lim)
+axs[0].set_ylim(-plot_lim, plot_lim)
+axs[0].set_aspect('equal', 'box')
+
+axs[1].plot(ee_positions_robosuite[:, 0], ee_positions_robosuite[:, 2])
+axs[1].set_xlabel("End effector x-position [m]")
+axs[1].set_ylabel("End effector z-position [m]")
+axs[1].set_xlim(-plot_lim, plot_lim)
+axs[1].set_ylim(0.0, 1.0)
+axs[1].set_aspect('equal', 'box')
+
+axs[0].scatter(ee_positions_robosuite[0, 0], ee_positions_robosuite[0, 1], color='g', label='Start')
+axs[1].scatter(ee_positions_robosuite[0, 0], ee_positions_robosuite[0, 2], color='g')
+
+axs[0].scatter(ee_positions_robosuite[-1, 0], ee_positions_robosuite[-1, 1], color='r', label='End')
+axs[1].scatter(ee_positions_robosuite[-1, 0], ee_positions_robosuite[-1, 2], color='r')
+
+axs[0].legend()  # To show labels for start and end points
+
+# Set common labels
+fig.suptitle(f"Total time taken: {num_steps * timestep_length} s")
+
+# Save the figure
 plt.savefig("figures/tracking_performance.png")
+
+
+# ----------------------------
+# --- Video ------------------
+# ----------------------------
+video_writer = imageio.get_writer("figures/video.mp4", fps=20)
+for idx, frame in enumerate(frames):
+    frame = frame.astype(np.uint8)
+    frame = cv2.flip(frame, 0)
+    cv2.putText(
+        frame,
+        f"t = {time_vec[idx]}s",
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2,
+        cv2.LINE_AA,
+    )
+
+    video_writer.append_data(frame)
+
+video_writer.close()
