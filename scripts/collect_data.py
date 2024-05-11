2dc8acbacc8f3adc63417a01ce39e2931d361685
fernandopalafox <fernandopalafox@utexas.edu>
2024-05-10 19:34:27 -0500

    new

--- scripts/collect_data.py
+++ scripts/collect_data.py
@@ -1,77 +1,41 @@
 import jax
 from jax import numpy as jp
-import robosuite as suite
-from robosuite.controllers import load_controller_config
 from brax.envs.panda import Panda
-import flax
 from brax.envs import State
-import numpy as np
+import flax
 import time
 import pickle
 from jax.config import config
+import jax.tree_util as jtu
 
+import matplotlib.pyplot as plt
 
 # -----------------------
 # --- Debug Utils -------
 # -----------------------
 # jax.config.update("jax_disable_jit", True)
-config.update("jax_enable_x64", True)
 
 
 # -----------------------
 # --- Sim Parameters ----
 # -----------------------
-num_steps = 2 ** 15
-friction_torque_coeff = 100.0
-friction_static = 10.0
+
+# Collection limits
+batch_size = 64
+num_batches = 500
 torque_logging_interval = 100
 num_joints = 7
-key = jax.random.key(0)
-key_states = jax.random.split(key, num=num_steps)
+seed = 0
 
-# -----------------------
-# --- Friction model ----
-# -----------------------
-
-
-# Friction torque proportional and opposite to joint velocity.
-# If any element of qd is zero, the corresponding friction torque is zero.
-def compute_friction_torques(q, qd):
-    return jp.where(qd != 0, -friction_torque_coeff * qd, friction_static)
-
-
-# -----------------------
-# --- Robosuite stuff ---
-# -----------------------
-
-print("Loading Robosuite environment...")
-start_time = time.time()
-
-# Environment configuration
-config = load_controller_config(default_controller="OSC_POSE")
-env_config = {}
-env_config["env_name"] = "Lift"
-env_config["robots"] = "Panda"
-env_config["camera_names"] = ["frontview"]
-env_config["camera_heights"] = 480
-env_config["camera_widths"] = 480
-env_config["control_freq"] = 10
-env_config["controller_configs"] = suite.load_controller_config(
-    default_controller="OSC_POSE"
+# Control params
+torque_max = 10.0
+torque_lims = jp.array([-torque_max, torque_max])
+joint_lims_max = jp.array(
+    [2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973]
 )
-env_config["has_renderer"] = False
-env_config["has_offscreen_renderer"] = False
-env_config["ignore_done"] = True
-env_config["use_camera_obs"] = False
-
-# Make the environment
-env_suite = suite.make(**env_config)
-
-# Reset the environment
-env_suite.reset()
-
-print("Robosuite environment loaded.")
-print(f"Time taken: {time.time() - start_time}")
+joint_lims_min = jp.array(
+    [-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973]
+)
 
 # -----------------------
 # --- Brax stuff --------
@@ -80,15 +44,17 @@ print(f"Time taken: {time.time() - start_time}")
 print("Loading Brax environment...")
 start_time = time.time()
 
-# Setup Brax environment
-seed = 0
-env_brax = Panda()
-env_reset_jitted = jax.jit(env_brax.reset)
-env_step_jitted = jax.jit(env_brax.step)
-low, high = env_suite.action_spec
+env = Panda()
+step_jitted = jax.jit(env.step_directly)
+reset_jitted = jax.jit(env.reset)
+friction_jitted = jax.jit(env.calculate_friction)
 
-print("Brax environment loaded.")
-print(f"Time taken: {time.time() - start_time}")
+print(f"Done. Time taken: {time.time() - start_time}")
+
+
+# ----------------------------
+# --- Collect data  ----------
+# ----------------------------
 
 
 # Data class
@@ -100,68 +66,73 @@ class MyData:
     next_state: State
 
 
-# ----------------------------
-# --- Generate OSC torques ---
-# ----------------------------
-print("Generating OSC torques...")
-start_time = time.time()
-
-torques = []
-for i in range(num_steps):
-    # Sample brax env
-    init_state = env_reset_jitted(key_states[i])
-
-    # Set robosuite env
-    env_suite.sim.data.qpos[env_suite.robots[0].joint_indexes] = (
-        init_state.pipeline_state.q
-    )
-    env_suite.sim.data.qvel[env_suite.robots[0].joint_indexes] = (
-        init_state.pipeline_state.qd
-    )
-
-    # Sample action and compute osc torques
-    action = np.random.uniform(low, high)  # use jax for this?
-    _, _, _, _ = env_suite.step(action)
-    torques_osc = env_suite.sim.data.ctrl
-
-    # Save torques
-    torques.append(torques_osc[0:num_joints])
-
-    if i % torque_logging_interval == 0:
-        print(f"Step {i} of {num_steps}")
-
-torques = jp.stack(torques)
-
-print("OSC torques generated.")
-print(f"Time taken: {time.time() - start_time}")
-
-# ----------------------------
-# --- Collect data  ----------
-# ----------------------------
-
-
 # Data collection function
-def make_data(key, torque):
+def make_data(key):
+    # Split key
+    rng1, rng2 = jax.random.split(key, num=2)
+
     # Sample init brax state and compute friction torques
-    init_state = env_reset_jitted(key)
-    friction = compute_friction_torques(
-        init_state.pipeline_state.q, init_state.pipeline_state.qd
-    )
+    init_state = reset_jitted(rng1)
+
+    # Sample random torque
+    torque = jax.random.uniform(
+                rng2,
+                (num_joints,),
+                minval=torque_lims[0],
+                maxval=torque_lims[1],
+            )
+
+    # Calculate friction
+    friction = friction_jitted(init_state)
 
     # Step in both environments
-    next_state = env_step_jitted(init_state, torque + friction)
+    next_state = step_jitted(init_state, torque + friction)
 
-    return MyData(init_state, torque, friction, next_state)
+    return MyData(
+        init_state,
+        torque,
+        friction,
+        next_state,
+    )
+
+
+# Data stacking function to append batches
+def tree_stack(trees):
+    return jtu.tree_map(lambda *v: jp.vstack(v), *trees)
 
 
 # Collect data
 print("Starting data collection loop...")
 start_time = time.time()
-data = jax.vmap(make_data)(key_states, torques)
-print("Data collection loop finished.")
-print(f"Time taken: {time.time() - start_time}"),
 
+batch_keys_init = jax.random.split(jax.random.key(seed), num=num_batches)
+batch_start_time = time.time()
+
+batch_keys_1 = jax.random.split(batch_keys_init[0], num=batch_size)
+data = jax.vmap(make_data)(batch_keys_1)
+
+print(
+        f"  Batch {1}/{num_batches}. Time taken: {time.time() - batch_start_time}"
+    )
+
+for batch_i, batch_key in enumerate(batch_keys_init[1:]):
+    batch_start_time = time.time()
+
+    batch_keys = jax.random.split(batch_key, num=batch_size)
+    batch_data = jax.vmap(make_data)(batch_keys)
+    data = tree_stack([data, batch_data])
+
+    print(
+        f"  Batch {batch_i + 2}/{num_batches}. Time taken: {time.time() - batch_start_time}"
+    )
+
+print(f"Done. Time taken: {time.time() - start_time}"),
 
 # Save data
-with open("data/data.pkl", "wb") as f:
+print("Saving data...")
+start_time = time.time()
+
+with open("data/data_panda.pkl", "wb") as f:
     pickle.dump(data, f)
+
+print(f"Done. Time taken: {time.time() - start_time}")
